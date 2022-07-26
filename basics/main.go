4e1723c009779aba52b3d435103ba1dfc5a3e131
compositefellow <compositefellow@protonmail.com>
2022-07-26 09:55:09 -0600

    user input

--- basics/main.go
+++ basics/main.go
@@ -8,20 +8,34 @@ func main() {
 	var remainingTickets uint8 = 50
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceName, remainingTickets, conferenceTickets)
+	fmt.Println("")
 
 	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Printf("We have %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend.")
+	fmt.Println("")
 
-	var userName string
-	var userTickets int
-	// ask user for name
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint8
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
-	
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets do you want:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets -= userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets.  You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("We have %v tickets remaining for %v \n", remainingTickets, conferenceName)
 
 }
-
 
